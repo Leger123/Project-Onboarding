dc98fe1a199e153a696d010d67644741e332f911
Kevin Dian <kevindian@hotmail.com>
2021-11-04 17:04:40 -0500

    Done first Java challenge

--- Week #1/Java/RPS/src/main/java/RPS.java
+++ Week #1/Java/RPS/src/main/java/RPS.java
@@ -1,2 +1,65 @@
 public class RPS {
+    public static String RPSFunction(String p1, String p2){
+        String result = "";
+
+        switch(p1){
+            case "rock":
+                switch (p2) {
+                    case "rock":
+                        result = "TIE";
+                        break;
+                    case "paper":
+                        result = "Player 2 wins";
+                        break;
+                    case "scissors":
+                        result = "Player 1 wins";
+                        break;
+                    default:
+                        result = "Invalid input, try again";
+                        break;
+                }
+                break;
+
+            case "paper":
+                switch (p2) {
+                    case "rock":
+                        result = "Player 1 wins";
+                        break;
+                    case "paper":
+                        result = "TIE";
+                        break;
+                    case "scissors":
+                        result = "Player 2 wins";
+                        break;
+                    default:
+                        result = "Invalid input, try again";
+                        break;
+                }
+                break;
+
+            case "scissors":
+                switch (p2) {
+                    case "rock":
+                        result = "Player 2 wins";
+                        break;
+                    case "paper":
+                        result = "Player 1 wins";
+                        break;
+                    case "scissors":
+                        result = "TIE";
+                        break;
+                    default:
+                        result = "Invalid input, try again";
+                        break;
+                }
+                break;
+
+            default:
+                result = "Invalid input, try again";;
+                break;
+        }
+
+
+        return result;
+    }
 }
---
+++ Week #1/Java/RPS/src/test/java/RPSTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RPSTest {
+
+    RPS rps;
+
+    public RPSTest() {
+    }
+
+    @BeforeEach
+    void setUp() {this.rps = new RPS();}
+
+    @Test
+    void RPSFunctionPlayerOneWins() {
+        String expectedResult = "Player 1 wins";
+        String actualResult = RPS.RPSFunction("rock","scissors");
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void RPSFunctionPlayerTwoWins() {
+        String expectedResult = "Player 2 wins";
+        String actualResult = RPS.RPSFunction("rock","paper");;
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void RPSFunctionPlayerTies() {
+        String expectedResult = "TIE";
+        String actualResult = RPS.RPSFunction("rock","rock");;
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void RPSFunctionError() {
+        String expectedResult = "Invalid input, try again";
+        String actualResult = RPS.RPSFunction("rock","apple");;
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
