71ba0e645495d967bb2bce6ec5f71fe96902ab89
Kevin Dian <kevindian@hotmail.com>
2021-11-05 20:38:55 -0500

    Finished First Javascript challenge

--- Week #1/JavaScript/js1.js
+++ Week #1/JavaScript/js1.js
@@ -10,29 +10,21 @@
     -Keep the relative order of the non-zero elements the same.
 */
 
-/* function zeroesToEnd(array){
-    let string = "";
-    for(let i = 0; i<array.length; i++){
+function zeroesToEnd(array){
+    let numOfZeroes = 0;
+    for(let i = 0; i<array.length-numOfZeroes; i++){
         if(array[i] == 0){
             array.splice(i,1);
-            array.push(0); 
+            array.push(0);
+            i--;
+            numOfZeroes++;
         }
-
-        string += array[i]+","
     }
-}  */
-
-function zeroesToEnd(arr) {
-    let q = 0;
-    for (let p=0; p<arr.length; p++) 
-        if (arr[p] != 0) arr[q++] = arr[p];
-    for (; q<arr.length; q++) arr[q] = 0;
-    return arr;
-}
+    return array;
+}  
 
 console.log(zeroesToEnd([1, 2, 0, 0, 4, 0, 5]));
 console.log(zeroesToEnd([0, 0, 2, 0, 5]));
 console.log(zeroesToEnd([4, 4, 5]));
 console.log(zeroesToEnd([0, 0]));
-
 
